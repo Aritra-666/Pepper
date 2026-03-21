0126a045ddb3c932bbe016be8df1955d63e05e26
Aritra-666 <aritrapandit67@gmail.com>
2026-03-22 02:07:40 +0530

    Testing all movements

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,153 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include "movements.h"
+#define SERVOMIN 150
+#define SERVOMAX 600
 
-// put function declarations here:
-int myFunction(int, int);
+//Limits of hip servos 
+
+#define HIPMAX 135 
+#define HIPMIN 60
+
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+int8_t AngErr[8] = {0, 0, 0, 0, 0, 0, 0, 0}; //ignoring for now
+
+int frameDelay = 200;
+
+int angleToPWM(int angle) {
+  return map(angle, 0, 180, SERVOMIN, SERVOMAX);
+}
+
+void setServoAngle(uint8_t channel, int angle){
+
+  int adjusted = constrain(angle + AngErr[channel], 0, 180);
+
+  if(channel%2 == 0) 
+    pwm.setPWM(channel, 0, angleToPWM(map(adjusted,0,180,HIPMIN,HIPMAX)));
+  else
+    pwm.setPWM(channel, 0, angleToPWM(180-adjusted));
+    
+
+}
+
+void robotDelay(unsigned long ms) {
+  unsigned long start = millis();
+  while (millis() - start < ms) {
+    delay(5);
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+    Wire.begin();
+    pwm.begin();
+    pwm.setPWMFreq(50);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("REST");
+  runRestPose();
+  delay(2000);
+ 
+  Serial.println("STAND");
+  runStandPose();
+  delay(2000);
+ 
+  Serial.println("WALK FWD");
+  runWalkPose();
+  delay(1000);
+ 
+  Serial.println("WALK BACK");
+  runWalkBackward();
+  delay(1000);
+ 
+  // Serial.println("TURN LEFT");
+  // runTurnLeft();
+  // delay(1000);
+ 
+  // Serial.println("TURN RIGHT");
+  // runTurnRight();
+  // delay(1000);
+ 
+  Serial.println("WAVE");
+  runWavePose();
+  delay(1000);
+ 
+  Serial.println("DANCE");
+  runDancePose();
+  delay(1000);
+ 
+  Serial.println("SWIM");
+  runSwimPose();
+  delay(1000);
+ 
+  Serial.println("POINT");
+  runPointPose();
+  delay(1000);
+ 
+  Serial.println("PUSHUP");
+  runPushupPose();
+  delay(1000);
+ 
+  Serial.println("BOW");
+  runBowPose();
+  delay(1000);
+ 
+  Serial.println("CUTE");
+  runCutePose();
+  delay(1000);
+ 
+  Serial.println("FREAKY");
+  runFreakyPose();
+  delay(1000);
+ 
+  Serial.println("WORM");
+  runWormPose();
+  delay(1000);
+ 
+  Serial.println("SHAKE");
+  runShakePose();
+  delay(1000);
+ 
+  Serial.println("SHRUG");
+  runShrugPose();
+  delay(1000);
+ 
+  Serial.println("DEAD");
+  runDeadPose();
+  delay(1000);
+ 
+  Serial.println("CRAB");
+  runCrabPose();
+  delay(1000);
+ 
+  Serial.println("FIGHT");
+  runFightPose();
+  delay(1000);
+ 
+  Serial.println("PUNCH");
+  runPunchPose();
+  delay(1000);
+ 
+  Serial.println("KICK");
+  runKickPose();
+  delay(1000);
+ 
+  Serial.println("DIZZY");
+  runDizzyPose();
+  delay(1000);
+ 
+  Serial.println("FALL");
+  runFallPose();
+  delay(1000);
+ 
+  Serial.println("GLITCH");
+  runGlitchPose();
+  delay(1000);
+ 
 }
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+ 
